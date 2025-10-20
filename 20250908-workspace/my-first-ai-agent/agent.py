fc1e4be43ed280c5d00edf92c59cfed56331f8e4
蔡晟郁 <tsaichengyu@caichengyudeMacBook-Air.local>
2025-10-20 13:55:12 +0800

    ai mood weather

--- 20250908-workspace/my-first-ai-agent/agent.py
+++ 20250908-workspace/my-first-ai-agent/agent.py
@@ -5,6 +5,9 @@ from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParamet
 import requests
 import os
 from dotenv import load_dotenv, dotenv_values
+# importing module
+#from geopy.geocoders import Nominatim
+#from timezonefinder import TimezoneFinder
 
 # Load environment variables from .env file
 load_dotenv()
@@ -61,81 +64,110 @@ def get_weather(city: str) -> dict:
         }
 
 
-def get_current_time(city: str) -> dict:
-    """Returns the current time in a specified city.
+def get_current_time(tz_identifier: str) -> dict:
+    """Returns the current time in a specified time zone identifier.
     Args:
-        city (str): The name of the city for which to retrieve the current time.
+        tz_identifier (str): The time zone identifier for which to retrieve the current time.
+        ex. New York -> "America/New_York", Taipei -> "Asia/Taipei", etc.
+        But sometimes the city name is not the same as the tz_identifier.
+        ex. Taoyuan -> "Asia/Taipei", Kaohsiung -> "Asia/Taipei", etc.
     Returns:
         dict: status and result or error msg.
     """
 
-    city_taiwan = ["taipei", "zhongli", "taoyuan", "kaohsiung", "taichung"]
+    # city_taiwan = ["taipei", "zhongli", "taoyuan", "kaohsiung", "taichung"]
 
-    if city.lower() == "new york":
-        tz_identifier = "America/New_York"
-    elif city.lower() in city_taiwan:
-        tz_identifier = "Asia/Taipei"
-    else:
+    # if city.lower() == "new york":
+    #     tz_identifier = "America/New_York"
+    # elif city.lower() in city_taiwan:
+    #     tz_identifier = "Asia/Taipei"
+    # else:
+    #     return {
+    #         "status": "error",
+    #         "error_message": (f"Sorry, I don't have timezone information for {city}."),
+    #     }
+    try:
+        tz = ZoneInfo(tz_identifier)
+        now = datetime.datetime.now(tz)
+        report = f'The current time is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
+        return {"status": "success", "report": report}
+    except Exception as e:
         return {
             "status": "error",
-            "error_message": (f"Sorry, I don't have timezone information for {city}."),
+            "error_message": f"An error occurred while fetching the current time: {str(e)}",
         }
 
-    tz = ZoneInfo(tz_identifier)
-    now = datetime.datetime.now(tz)
-    report = f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
-    return {"status": "success", "report": report}
 
-def ncu_calender(date: str) -> dict:
-    """
-    查詢中央大學行事曆特定日期的資訊
-    Args:
-        date (str): 日期格式 'YYYY-MM-DD'
-    Returns:
-        dict: status and result or error msg.
-    """
-    the_calendar = {
-        "2024-09-29": "教師節補假",
-        "2024-10-06": "中秋節放假"
-    }
-    
-    if date in the_calendar:
-        return {
-            "status": "success", 
-            "report": f"{date} 的行事曆資訊: {the_calendar[date]}"
-        }
-    else:
-        return {
-            "status": "error", 
-            "error_message": f"抱歉，{date} 沒有特別的行事曆資訊。"
+# def get_current_time(city: str) -> dict:
+#     """Returns the current time in a specified city.
+#     Args:
+#         city (str): The name of the city for which to retrieve the current time.
+#     Returns:
+#         dict: status and result or error msg.
+#     """
+#     # initialize Nominatim API
+#     geolocator = Nominatim(user_agent="geoapiExercises")
 
-        }
-    
+#     # input as a geek
+#     # lad = "Dhaka"
+#     print("Location address:", city)
+
+#     # getting Latitude and Longitude
+#     location = geolocator.geocode(city)
+
+#     print("Latitude and Longitude of the said address:")
+#     print((location.latitude, location.longitude))
+
+#     # pass the Latitude and Longitude
+#     # into a timezone_at
+#     # and it return timezone
+#     obj = TimezoneFinder()
+
+#     # returns 'Europe/Berlin'
+#     result = obj.timezone_at(lng=location.longitude, lat=location.latitude)
+#     print("Time Zone : ", result)
+#     try:
+#         tz = ZoneInfo(result)
+#         now = datetime.datetime.now(tz)
+#         report = f'The current time is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
+#         return {"status": "success", "report": report}
+#     except Exception as e:
+#         return {
+#             "status": "error",
+#             "error_message": f"An error occurred while fetching the current time: {str(e)}",
+#         }
+
 root_agent = LlmAgent(
     name="weather_time_agent",
-    model="gemini-2.0-flash-lite",
+    model="gemini-2.5-flash",
     description=("Agent to answer questions about the time and weather in a city."),
     instruction=(
         """
         You are a helpful agent who can answer user questions about the time and weather in a city.
         When you call a tool, you must use English for the city name.
-        請用繁體中文回答問題。a
-        你也可以查詢中央大學行事曆，輸入日期格式 YYYY-MM-DD。
+        請用繁體中文回答問題。
         """
     ),
     tools=[
-        get_weather, 
+        get_weather,
         get_current_time,
-        ncu_calender,
         MCPToolset(
+            # Use StdioServerParameters for local process communication
             connection_params=StdioServerParameters(
-                command="npx",
+                command="/Users/tsaichengyu/.local/bin/uv",
                 args=[
-                    "-y",
-                    "@modelcontextprotocol/server-filesystem",
-                    "/Users/tsaichengyu/Documents/Projects/ai/test_file_share_20250922",
-                ],
+                    "--directory",
+                    "/Users/tsaichengyu/Documents/Projects/ai/20251013-weather2mood",
+                    "run",
+                    "server.py",
+                ]
             ),
+            # tool_filter=[
+            #     "read_file",
+            #     "list_directory",
+            # ],  # Optional: filter specific tools
+            # For remote servers, you would use SseServerParams instead:
+            # connection_params=SseServerParams(url="http://remote-server:port/path", headers={...})
         ),
     ],
 )
